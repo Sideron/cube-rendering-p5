e9abbf904614ac087f62872e82dd7d8cd47406d6
Sideron <66834276+Sideron@users.noreply.github.com>
2026-02-17 14:28:42 -0500

    Refactor Node and update setup function for 3D effect

--- sketch.js
+++ sketch.js
@@ -14,29 +14,44 @@ function addObject(newObject){
 
 function setup() {
   createCanvas(WIDTH, HEIGTH);
-  n1 = addObject(new Node(WIDTH/4,HEIGTH/4,null))
-  n2 = addObject(new Node(WIDTH*3/4,HEIGTH/4,n1))
-  n3 = addObject(new Node(WIDTH*3/4,HEIGTH*3/4,n2))
-  n4 = addObject(new Node(WIDTH/4,HEIGTH*3/4,n3))
+  n1 = addObject(new Node(-150,-150,1.5,null))
+  n2 = addObject(new Node(150,-150,1.5,n1))
+  n3 = addObject(new Node(150,150,1.5,n2))
+  n4 = addObject(new Node(-150,150,1.5,n3))
   n1.setNext(n4)
+  
+  n1c = addObject(new Node(-150,-150,0.5,null))
+  n2c = addObject(new Node(150,-150,0.5,n1c))
+  n3c = addObject(new Node(150,150,0.5,n2c))
+  n4c = addObject(new Node(-150,150,0.5,n3c))
+  n1c.setNext(n4c)
 }
 
 function draw() {
   background(0);
-  objs.forEach(x => {x.draw()})
+  objs.forEach(point => {
+    point.z+=0.1
+    point.draw()
+  })
 }
 
-function Node(x,y,next){
-  this.x = x
-  this.y = y
+function Node(x,y,z,next){
+  this.cx = x
+  this.cy = y
+  this.z = z
+  this.x = this.cx/this.z
+  this.y = this.cy/this.z
   this.next = next
   
   this.draw = function(){
+    this.x = this.cx/this.z
+    this.y = this.cy/this.z
     fill("white")
-    circle(this.x,this.y,10)
+    circle(this.x+WIDTH/2,this.y+HEIGTH/2,10)
     if(this.next){
       stroke("white")
-      line(this.x,this.y,this.next.x,this.next.y)
+      line(this.x+WIDTH/2,this.y+HEIGTH/2,
+           this.next.x+WIDTH/2,this.next.y+HEIGTH/2)
     }
   }
   
